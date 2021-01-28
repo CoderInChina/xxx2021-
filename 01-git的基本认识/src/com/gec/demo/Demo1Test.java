f14b1f69772c0a4ae0a057d28da67d53b9ecf2a9
xmq <123@qq.com>
2021-01-28 22:05:51 +0800

    张三删除了一些废物，然后提交了

--- 01-git的基本认识/src/com/gec/demo/Demo1Test.java
+++ 01-git的基本认识/src/com/gec/demo/Demo1Test.java
@@ -2,18 +2,8 @@ package com.gec.demo;
 
 public interface Demo1Test {
 	
+
 	
-	
-	void zhangsan();
-	
-	void lisi_bb();
-	
-	// 张三的xxx方法
-	void zhangsan_xxx();
-	
-	
-	// 张三加了一个 test_brank 分支
-	void test_brank();
 	
 
 }
